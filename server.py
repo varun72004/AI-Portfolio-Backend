a3ec41238af106170c519379db3304d2d8e99077
Dheeraj8580 <dheerajk.jk@gmail.com>
2026-05-23 02:00:15 +0530

    update

--- server.py
+++ server.py
@@ -2227,3 +2227,9 @@ async def startup_event():
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
